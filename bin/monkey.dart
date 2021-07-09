b6fb618b844c15f8b1505b6449f2b6e41288e5a3
ktanaka101 <kentanaka101@gmail.com>
2021-07-10 00:29:06 +0900

    initial commit

---
+++ bin/monkey.dart
@@ -0,0 +1,5 @@
+import 'package:monkey/monkey.dart' as monkey;
+
+void main(List<String> arguments) {
+  print('Hello world: ${monkey.calculate()}!');
+}
---
+++ test/monkey_test.dart
@@ -0,0 +1,8 @@
+import 'package:monkey/monkey.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
