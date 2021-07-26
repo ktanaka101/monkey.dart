223ad30201aa686eb7e0d11c9fa18593497e1649
ktanaka101 <kentanaka101@gmail.com>
2021-07-27 00:32:00 +0900

    wip

--- test/monkey/parser_test.dart
+++ test/monkey/parser_test.dart
@@ -160,7 +160,55 @@ void main() {
     });
   });
 
-  test('operator precedence parsing', () {});
+  test('operator precedence parsing', () {
+    final inputs = [
+      ['-a * b', '((-a) * b);'],
+      ['!-a', '(!(-a));'],
+      ['a + b + c', '((a + b) + c);'],
+      ['a + b - c', '((a + b) - c);'],
+      ['a * b * c', '((a * b) * c);'],
+      ['a * b / c', '((a * b) / c);'],
+      ['a + b / c', '(a + (b / c));'],
+      ['a + b * c + d / e - f', '(((a + (b * c)) + (d / e)) - f);'],
+      ['3 + 4; -5 * 5', '(3 + 4);((-5) * 5);'],
+      ['5 > 4 == 3 < 4', '((5 > 4) == (3 < 4));'],
+      ['5 < 4 != 3 > 4', '((5 < 4) != (3 > 4));'],
+      [
+        '3 + 4 * 5 == 3 * 1 + 4 * 5',
+        '((3 + (4 * 5)) == ((3 * 1) + (4 * 5)));',
+      ],
+      ['true', 'true;'],
+      ['false', 'false;'],
+      ['3 > 5 == false', '((3 > 5) == false);'],
+      ['3 < 5 == true', '((3 < 5) == true);'],
+      ['1 + (2 + 3) + 4', '((1 + (2 + 3)) + 4);'],
+      ['(5 + 5) * 2', '((5 + 5) * 2);'],
+      ['2 / (5 + 5)', '(2 / (5 + 5));'],
+      ['-(5 + 5)', '(-(5 + 5));'],
+      ['!(true == true)', '(!(true == true));'],
+      ['a + add(b * c) + d', '((a + add((b * c))) + d);'],
+      [
+        'add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8));',
+        'add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)));',
+      ],
+      [
+        'add(a + b + c * d / f  + g)',
+        'add((((a + b) + ((c * d) / f)) + g));',
+      ],
+      [
+        'a * [1, 2, 3, 4][b * c] * d',
+        '((a * ([1, 2, 3, 4][(b * c)])) * d);',
+      ],
+      [
+        'add(a * b[2], b[1], 2 * [1, 2][1])',
+        'add((a * (b[2])), (b[1]), (2 * ([1, 2][1])));',
+      ],
+    ].map((input) => Tuple2<String, String>(input[0], input[1])).toList();
+
+    runTest<String>(inputs, (program, expected) {
+      expect(program.toString(), expected);
+    });
+  });
 }
 
 void testLetByStmt(ast.Stmt actual, ast.Let expected) {
