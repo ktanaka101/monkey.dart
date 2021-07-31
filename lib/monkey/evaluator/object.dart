2c296cd78b6b9d5aa8dc98ccb5fb74fc9b6c29ba
ktanaka101 <kentanaka101@gmail.com>
2021-07-31 20:32:42 +0900

    wip

--- lib/monkey/evaluator/object.dart
+++ lib/monkey/evaluator/object.dart
@@ -6,3 +6,9 @@ class Array extends Object {
   Array(this.elements);
   List<Object> elements;
 }
+
+class Boolean extends Object with Hashable {
+  // ignore: avoid_positional_boolean_parameters
+  Boolean(this.value);
+  bool value;
+}
