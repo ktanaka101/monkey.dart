84d3604141ae94e99b959bd978b9770aa0a2a7c3
ktanaka101 <kentanaka101@gmail.com>
2021-08-01 22:07:26 +0900

    wip

--- lib/monkey/evaluator/object.dart
+++ lib/monkey/evaluator/object.dart
@@ -1,3 +1,6 @@
+import 'package:monkey/monkey/evaluator/env.dart';
+import 'package:monkey/monkey/parser/ast.dart' as ast;
+
 abstract class Object {}
 
 mixin Hashable {}
@@ -11,4 +14,11 @@ class Boolean extends Object with Hashable {
   // ignore: avoid_positional_boolean_parameters
   Boolean(this.value);
   bool value;
+}
+
+class MFunction {
+  MFunction(this.params, this.body, this.env);
+  List<ast.Ident> params;
+  ast.Block body;
+  Environment env;
 }
