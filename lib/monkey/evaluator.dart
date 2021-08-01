a17f57e24608032dbb07d8f43e055e730ba39483
ktanaka101 <kentanaka101@gmail.com>
2021-08-01 23:07:47 +0900

    wip

--- lib/monkey/evaluator.dart
+++ lib/monkey/evaluator.dart
@@ -27,3 +27,33 @@ object.Object _evalStmt(ast.Stmt stmt, Environment env) {
     throw Exception('Unreachable');
   }
 }
+
+object.Object _evalExpr(ast.Expr expr, Environment env) {
+  if (expr is ast.Array) {
+    throw Exception('unimplements');
+  } else if (expr is ast.Boolean) {
+    throw Exception('unimplements');
+  } else if (expr is ast.Int) {
+    throw Exception('unimplements');
+  } else if (expr is ast.StringLit) {
+    return object.StringLit(expr.value);
+  } else if (expr is ast.Hash) {
+    throw Exception('unimplements');
+  } else if (expr is ast.Ident) {
+    throw Exception('unimplements');
+  } else if (expr is ast.InfixExpr) {
+    throw Exception('unimplements');
+  } else if (expr is ast.PrefixExpr) {
+    throw Exception('unimplements');
+  } else if (expr is ast.If) {
+    throw Exception('unimplements');
+  } else if (expr is ast.MFunction) {
+    throw Exception('unimplements');
+  } else if (expr is ast.Index) {
+    throw Exception('unimplements');
+  } else if (expr is ast.Call) {
+    throw Exception('unimplements');
+  } else {
+    throw Exception('Unreachable');
+  }
+}
