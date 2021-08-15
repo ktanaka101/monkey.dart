0313a09716e99348caba185b62131cfa97df658f
ktanaka101 <kentanaka101@gmail.com>
2021-08-15 19:54:17 +0900

    wip

--- lib/monkey.dart
+++ lib/monkey.dart
@@ -1,3 +1,13 @@
-int calculate() {
-  return 6 * 7;
+import './monkey/ast.dart' as ast;
+import './monkey/lexer.dart' as lexer;
+import './monkey/parser.dart' as parser;
+
+export './monkey/evaluator.dart' show defineMacros, extendMacros, eval;
+export './monkey/evaluator/env.dart' show Environment;
+export './monkey/evaluator/object.dart';
+
+ast.Program buildAst(String input) {
+  final l = lexer.Lexer(input);
+  final p = parser.Parser(l);
+  return p.parseProgram();
 }
