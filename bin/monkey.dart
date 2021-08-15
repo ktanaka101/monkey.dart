8d3b0631f1862a4ae4dd945d509f74bc00ae2a5a
ktanaka101 <kentanaka101@gmail.com>
2021-08-15 19:54:23 +0900

    wip

--- bin/monkey.dart
+++ bin/monkey.dart
@@ -1,5 +1,39 @@
+import 'dart:io';
 import 'package:monkey/monkey.dart' as monkey;
+import 'package:monkey/monkey/error.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${monkey.calculate()}!');
+const prompt = '>> ';
+
+Future<void> main(List<String> arguments) async {
+  final env = monkey.Environment();
+  final macroEnv = monkey.Environment();
+
+  // ignore: literal_only_boolean_expressions
+  while (true) {
+    stdout.write(prompt);
+
+    final input = stdin.readLineSync();
+    if (input == null) {
+      continue;
+    }
+
+    if (input == 'exit') {
+      break;
+    }
+
+    try {
+      final ast = monkey.buildAst(input);
+      monkey.defineMacros(ast, env);
+      final expanded = monkey.extendMacros(ast, macroEnv);
+
+      final evaluated = monkey.eval(expanded, env);
+      if (evaluated is monkey.Null) {
+        continue;
+      }
+      print('$evaluated');
+    } on MonkeyException catch (e) {
+      print(e.msg);
+      continue;
+    }
+  }
 }
